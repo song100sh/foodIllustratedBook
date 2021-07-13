7ad93b9597c225786480c0cd882074e0e941dc53
song100sh <song100sh@gmail.com>
2021-07-13 18:31:16 +0900

    localstorage 저장기능 추가

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 'use strict';
 
-const subway = document.querySelector('#subwayName');
+const onClickRememberMe = () => {
+  const locationDir = document.querySelector('#locationName');
+  setCookie(locationDir.value);
+}
+
+const setCookie = (location) => {
+  let rememberData = {
+    where: location 
+  };
+  
+  localStorage.setItem(REMEMBER_LOCAL, JSON.stringify(rememberData));
+}
+
+const getCookie = () => {
+  const getData = localStorage.getItem(REMEMBER_LOCAL);
+  if (getData === null) {
+    return null;
+  }
+  
+  const parseData = JSON.parse(getData);
+  return parseData.where;
+}
+
 const submitBtn = document.querySelector('#login_submit');
 
 submitBtn.addEventListener('click', () => {
-  const subwayValue = subway.value;
   if (subwayValue === "") {
     alert("지하철, 정류장을 입력해주세요");
   } else {
